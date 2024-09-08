31cc5ef4568d5826509c39b0a62b8894b46a03e1
Iryna Mykhailovska <bagira.irina.m@gmail.com>
2024-09-08 18:13:02 +0300

    add

---
+++ src/components/App/App.jsx
@@ -0,0 +1,56 @@
+import { lazy, Suspense } from 'react';
+
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { RestrictedRoute } from '../RestrictedRoute/RestrictedRoute';
+import { SharedLayout } from '../SharedLayout/SharedLayout';
+import { PrivateRoute } from '../PrivateRoute/PrivateRoute';
+
+import { Toaster } from 'react-hot-toast';
+import './App.css';
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const SignupPage = lazy(() => import('../../pages/SignupPage/SignupPage'));
+const SigninPage = lazy(() => import('../../pages/SigninPage/SigninPage'));
+const WelcomePage = lazy(() => import('../../pages/WelcomePage/WelcomePage'));
+const NotFoundPage = lazy(() =>
+  import('../../pages/NotFoundPage/NotFoundPage')
+);
+
+export default function App() {
+  return (
+    <div>
+      <SharedLayout>
+        <Suspense fallback={<Toaster />}>
+          <Routes>
+            <Route path="/welcome" element={<WelcomePage />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute component={HomePage} redirectTo="/welcome" />
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <RestrictedRoute component={SignupPage} redirectTo="/home" />
+              }
+            />
+            <Route
+              path="/signin"
+              element={
+                <RestrictedRoute component={SigninPage} redirectTo="/home" />
+              }
+            />
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute component={HomePage} redirectTo="/signup" />
+              }
+            />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </SharedLayout>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App/App.jsx';
+import 'modern-normalize';
+import './styles/globalStyles.css';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { PersistGate } from 'redux-persist/integration/react';
+import Loader from './components/Loader/Loader.jsx';
+import { persistor, store } from './redux/store';
+
+import { Provider } from 'react-redux';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <BrowserRouter basename="/project-07-react-node">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
 );
